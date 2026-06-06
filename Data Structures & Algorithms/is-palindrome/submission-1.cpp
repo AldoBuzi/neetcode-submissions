b9ab701a0805aab25315a6ad0591a5025060353d
Donaldo Buzi <89732962+AldoBuzi@users.noreply.github.com>
2026-06-06 16:36:29 +0200

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string new_string = "";
+        for(int i= 0; i < s.size(); i++){
+            if(s[i] == ' ') continue;
+            if(std::isalnum(s[i])){
+                new_string += std::tolower(s[i]);
+            }
+        }
+        cout << new_string;
+        string copy = new_string;
+        std::reverse(new_string.begin(), new_string.end());
+        return copy == new_string;
+    }
+};
